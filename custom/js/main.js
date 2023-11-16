d977660ec0eb1c3b412f50453c91c918232bd3ec
RekaMel <63839716+RekaMel@users.noreply.github.com>
2023-11-16 14:22:51 +0100

    Adding custom control from OpenLayers example

--- custom/js/main.js
+++ custom/js/main.js
@@ -1,3 +1,50 @@
+//Custom Control
+
+/**
+ * Define a namespace for the application.
+ */
+window.app = {};
+var app = window.app;
+
+//
+// Define rotate to north control.
+//
+
+/**
+ * @constructor
+ * @extends {ol.control.Control}
+ * @param {Object=} opt_options Control options.
+ */
+app.DrawingApp = function (opt_options) {
+  var options = opt_options || {};
+
+  var button = document.createElement("button");
+  button.innerHTML = "N";
+
+  var this_ = this;
+  var startStopApp = function () {
+    //this_.getMap().getView().setRotation(0);
+    alert("You clicked control");
+  };
+
+  button.addEventListener("click", startStopApp, false);
+  button.addEventListener("touchstart", startStopApp, false);
+
+  var element = document.createElement("div");
+  element.className = "draw-app ol-unselectable ol-control";
+  element.appendChild(button);
+
+  ol.control.Control.call(this, {
+    element: element,
+    target: options.target,
+  });
+};
+ol.inherits(app.DrawingApp, ol.control.Control);
+
+//
+// Create map, giving it a rotate to north control.
+//
+
 // View
 
 var myview = new ol.View({
@@ -17,6 +64,14 @@ var layerArray = [baseLayer];
 
 //Map
 var map = new ol.Map({
+  controls: ol.control
+    .defaults({
+      attributionOptions: {
+        collapsible: false,
+      },
+    })
+    .extend([new app.DrawingApp()]),
+
   target: "mymap",
   view: myview,
   layers: layerArray,
